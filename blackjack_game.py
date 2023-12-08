778c57fc2b85ff489b580fd01eadfb9b02f29fb2
Alice <vph.sevi@gmail.com>
2023-12-08 20:35:50 +0100

    Terminado

--- blackjack_game.py
+++ blackjack_game.py
@@ -1,9 +1,13 @@
+from colorama import Fore, Back, Style
+from termcolor import colored
+
+
 import random 
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10,
          'Queen':10, 'King':10, 'Ace':11}
-playing = True
+winner = False
 
 #Create cards
 
@@ -67,141 +71,142 @@ class Chip ():
     def lose(self):
         self.total -= self.bet
                 
+                
+                
 def new_bet (chips):
     while chips.bet == 0 or chips.bet > chips.total:
         try:
             chips.bet = int(input("Cuál es tu apuesta? "))
             if chips.bet > chips.total:
-                print (f"No hay pasta, te queda {chips.total}")
+                print (f"Saldo insuficiente. Te queda {chips.total}")
         except:
-            print ("Escribe bien, coño!!!")
+            print ("La cantidad no es correcta")
     return chips.bet
             
 class Game():
     def __init__(self):
-        self.new_deck = Deck()
-        self.player_hand = Hand()
-        self.dealer_hand = Hand()
         self.player_chip = Chip()
+        self.winner = False
 
     def game_setup(self):
+        self.winner = False
         self.new_deck = Deck()
         self.new_deck.shuffle()
+        
+        self.player_hand = Hand()
+        self.dealer_hand = Hand()
+        
         new_bet(self.player_chip)
+        
         self.player_hand.add_card(self.new_deck.deal())
         self.player_hand.add_card(self.new_deck.deal())
         self.dealer_hand.add_card(self.new_deck.deal())
         self.dealer_hand.add_card(self.new_deck.deal())
-        print("\nPlayer")
-        print(self.player_hand.value)
-        for cards in self.player_hand.card:
-            print(cards)
-        print("\nDealer")
-        print(self.dealer_hand.value)
-        for cards in self.dealer_hand.card[1:]:
-            print(cards)
-            print("\n")
-        
-        
+
+        self.print_table("player")
+        return
+
+            
+
     def start_game_player(self):
-        while self.ask_new_card == "Y":  
-            self.player_hand.add_card(self.new_deck.deal())  
-            self.print_table(turno = "player")  
-            self.check_continue()    
+        self.player_hand.value = 21
+        if self.player_hand.value == 21:
+            self.check_winner()
+        else:
+            self.response = input("Quieres una nueva carta? S/N ")
+            while self.response.upper() == "S":
+                self.player_hand.add_card(self.new_deck.deal())
+                self.print_table(turno = "player")  
+                if self.player_hand.value >= 21:
+                    self.check_winner()
+                    if self.winner == True:
+                        break
+                self.response = input("Quieres una nueva carta? S/N ")
                      
             
     def start_game_dealer(self):
+
         while self.dealer_hand.value <= 17:       
             self.dealer_hand.add_card(self.new_deck.deal())     
             self.print_table(turno = "dealer")  
-        self.check_continue()   
+            self.check_winner()
      
      
     def print_table(self,turno):
-        print("\nPlayer")
-        print(self.player_hand.value)
+        print(colored("\nPlayer", 'red'))
+        print(colored(self.player_hand.value, 'yellow'))
+        
         for cards in self.player_hand.card:
             print(cards)
-        print("\nDealer")
-        print(self.dealer_hand.value)
+        print(colored("\nDealer", 'green'))
+        
         if turno == "player":
             for cards in self.dealer_hand.card[1:]:
                 print(cards)
         else:
+            print(colored(self.dealer_hand.value,'yellow'))
             for cards in self.dealer_hand.card:
                 print(cards)
         print("\n")
+        return
 
-            
-    def ask_new_card(self):
-            response = ""
-            while self.player_hand.value < 21:
-                
-                response = input("Quieres una nueva carta? S/N ")
-                
-                if response.upper() == "S":
-                    self.player_hand.add_card(self.new_deck.deal())
-                    print(self.player_hand.value)
-                    for cards in self.player_hand.card:
-                        print(cards)
-                    print("\nDealer")
-                    for cards in self.dealer_hand.card[1:]:
-                        print(cards)
-                    print("\n")
-            
-                elif response.upper() == "N":
-                    while self.dealer_hand.value < 17:
-                        self.dealer_hand.add_card(self.new_deck.deal())
-                        print(self.player_hand.value)
-                        for cards in self.player_hand.card:
-                            print(cards)
-                        print("\nDealer")
-                        print(self.dealer_hand.value)
-                        for cards in self.dealer_hand.card:
-                            print(cards)
-                        print("\n")       
-                        break    
-                    
-    def check_continue(self):
-                
-                if self.player_hand.value > 21:
-                    print("Te has pasado. La máquina gana")
-                    self.player_chip.lose()
-                    return False
-                
-                elif self.dealer_hand.value == 21:
-                    print("La máquina gana")
-                    self.player_chip.lose()
-                    return False 
-                
-                elif self.player_hand.value == 21:
-                    print("Has ganado")
-                    self.player_chip.win()
-                    return False         
-                
-                elif self.dealer_hand.value > 21:
-                    print("Has ganado")
-                    self.player_chip.win()               
-                    return False 
-                
-                elif self.player_hand.value > self.dealer_hand.value and self.dealer_hand.value >= 17:
-                    print("Has ganado")
-                    self.player_chip.win()   
-                    return False 
-                
-                elif self.dealer_hand.value > self.player_hand.value:
-                    print("La máquina gana")
-                    self.player_chip.win()
-                    return False 
-                
-                print(f"Tu nuevo saldo es: {self.player_chip.total}")    
-                self.new_deck = Deck()
-                self.new_deck.shuffle()
-                self.player_hand = Hand()
-                self.dealer_hand = Hand()            
-                            
-                    
-"""-------Juego---------"""
 
+                    
+    def check_winner(self):
+        while self.winner == False:
+            if self.player_hand.value > 21:
+                print(colored ("Te has pasado. La máquina gana", 'light_magenta'))
+                self.player_chip.lose()
+                self.winner = True
+            
+            elif self.dealer_hand.value == 21:
+                print(colored ("La máquina gana", 'light_magenta'))
+                self.player_chip.lose()
+                self.winner = True
+            
+            elif self.player_hand.value == 21:
+                print(colored ("Has ganado", 'light_magenta'))
+                self.player_chip.win()
+                self.winner = True        
+            
+            elif self.dealer_hand.value > 21:
+                print(colored ("Has ganado", 'light_magenta'))
+                self.player_chip.win()               
+                self.winner = True
+            
+            elif self.player_hand.value > self.dealer_hand.value and self.dealer_hand.value >= 17:
+                print(colored ("Has ganado", 'light_magenta'))
+                self.player_chip.win()   
+                self.winner = True
+            
+            elif self.dealer_hand.value > self.player_hand.value:
+                print(colored ("La máquina gana", 'light_magenta'))
+                self.player_chip.lose()
+                self.winner = True
+                
+            elif self.dealer_hand.value == self.player_hand.value:
+                print(colored ("Empate", 'light_magenta'))
+                self.winner = True
+            else:
+                return
+        print(f"Tu nuevo saldo es: {self.player_chip.total}")    
+
+             
+                        
 new_game = Game()
-new_game.game_setup()
+
+while new_game.player_chip.total>0:
+    new_game.game_setup()
+    new_game.start_game_player()
+    new_game.start_game_dealer()
+    new_game.player_chip.bet = 0
+ 
+
+
+                    
+
+
+
+
+
+    
